e412023b9bf0ff7bcc5b880bef26087390b23ca9
xTheSnopx <xjuanesh@gmail.com>
2025-08-01 16:11:41 -0500

    final-bootcam

--- SoulsWeapons/src/app/app.routes.ts
+++ SoulsWeapons/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { DashboardPlay } from './components/dashboard-play/dashboard-play';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+    path: '',
+    component: Home, // Se carga al inicio
+},
+//   {
+//     path: 'about',
+//     component: AboutComponent,
+//   },
+{
+    path: 'dashboardGame',
+    component: DashboardPlay
+},
+{
+  path: '',
+  component: Home,
+  pathMatch: 'full'
+}
+
+
+];
+
+
---
+++ SoulsWeapons/src/app/components/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+  constructor(private router: Router){}
+
+  playGame(){
+  
+    this.router.navigate(['dashboardGame']);
+    console.log("iniciando juego")
+  }
+}
